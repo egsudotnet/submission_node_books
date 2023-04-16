bc4a3e1635995a1036db658632eacbb26ebb3366
MSI\Egih Sugiatna <egsudotnet@gmail.com>
2023-04-16 20:21:39 +0700

    first commit

---
+++ src/routes.js
@@ -0,0 +1,44 @@
+const {
+  addBookHandler,
+  getAllBooksHandler,
+  getBookByIdHandler,
+  getBookByNameHandler,
+  editBookByIdHandler,
+  deleteBookByIdHandler,
+} = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBookHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBooksHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books/{id}',
+    handler: getBookByIdHandler,
+  },
+  {
+    method: 'GET',
+    path: '/book',
+    handler: getBookByNameHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/books/{id}',
+    handler: editBookByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{id}',
+    handler: deleteBookByIdHandler,
+ },
+];
+  
+
+module.exports = routes;
